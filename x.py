6f058d20f2edc9b00c6f5b0705df0fbe606f495d
RishabhPIET <94277218+RishabhPIET@users.noreply.github.com>
2023-09-07 11:24:14 +0530

    Create x.py

---
+++ x.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.title('Welcome To :violet[learn And Build]')
+
+choice= st.selectbox('Login/Signup',['Login','Sign Up'])
+if choice=='Login':
+    emaill=st.text_input('Email Adddress')
+    Password=st.text_input('Password',type='password')
+    
+    st.button('Login')
+
+else:
+    emaill=st.text_input('Email Adddress')
+    First_Name=st.text_input('First Name')
+    Last_Name=st.text_input('Last Name')
+    Password=st.text_input('Password',type='password')
+    st.button('Sign Up')
